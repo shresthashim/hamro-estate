c9e967e0216688d73c61a2139a30f9f5d04d9478
AsHim Shrestha <ashimsth89@gmail.com>
2023-11-25 20:18:42 +0545

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB.");
+  })
+  .catch((err) => {
+    console.log("Error in connection in database.");
+  });
 
 const app = express();
 
