940d12234e096932314a99d99b854578d7d56ca5
AsHim Shrestha <ashimsth89@gmail.com>
2023-11-25 10:34:55 +0545

    adding pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/Signin";
 const App = () => {
   return (
     <div>
-      <h1 className='text-red-400'>AsHim</h1>
-    </div>
-  )
-}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<Home/>} />
+          <Route path='/sign-in' element = {<SignIn/>} />
+          <Route path='/sign-up' element = {<SignUp/>} />
+          <Route path='/about' element = {<About/>} />
+          <Route path='/profile' element = {<Profile/>} />
+        </Routes>
+      </BrowserRouter>
 
-export default App
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div>
+      Sigin
+    </div>
+  )
+}
+
+export default SignIn
