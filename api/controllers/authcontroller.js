6ef543288a6d400a9f77d9f457bc5f535c8a986c
AsHim Shrestha <ashimsth89@gmail.com>
2023-11-26 16:04:01 +0545

    create sign in api route

--- api/controllers/authcontroller.js
+++ api/controllers/authcontroller.js
@@ -1,5 +1,7 @@
 const User = require("../models/usermodel");
 const bcryptjs = require("bcryptjs");
+const { errorHandler } = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,4 +16,23 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = { signup };
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid Credentials!"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin };
--- api/routes/authroute.js
+++ api/routes/authroute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signup } = require("../controllers/authcontroller");
+const { signup, signin } = require("../controllers/authcontroller");
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 module.exports = router;
