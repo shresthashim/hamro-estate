9fb99e1dc97f5f46e3e4691b078b9ffba7029262
AsHim Shrestha <ashimsth89@gmail.com>
2023-11-29 17:59:29 +0545

    add show listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -46,6 +46,8 @@ const Profile = () => {
   const [updateSuccess, setUpdateSuccess] = useState(false);
   const [isDeleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
   const [isSignOutConfirmationOpen, setSignOutConfirmationOpen] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
@@ -154,6 +156,21 @@ const Profile = () => {
     );
   };
 
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -221,6 +238,30 @@ const Profile = () => {
 
       <p className='text-red-700 mt-5'>{error ? error : ""}</p>
       <p className='text-green-700'>{updateSuccess ? "User is Updated Successfully!" : ""}</p>
+      <button className='text-green-700 w-full' onClick={handleShowListings}>
+        Show Listings
+      </button>
+      <p className='text-red-700 mt-5'>{showListingsError ? "Error showing listings!" : ""}</p>
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className='text-center  text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+              <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} alt='listings' className='h-16 w-16 object-contain' />
+              </Link>
+
+              <Link to={`/listing/${listing._id}`} className='text-slate-700 font-semibold flex-1 hover:underline truncate'>
+                <p>{listing.name}</p>
+              </Link>
+              <div className='flex flex-col items-center'>
+                <button className='text-red-700 uppercase'>Delete</button>
+                <button className='text-green-700 uppercase'>Edit</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
 
       {/* Confirmation Modals */}
       <ConfirmationModal
