7a772c1acc0367021900348594427b1f0af91914
AsHim Shrestha <ashimsth89@gmail.com>
2023-12-10 09:52:23 +0545

    import SignIn corrected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
-import SignIn from "./pages/Signin";
+import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
 import PrivateRoute from "./components/privateRoute";
 import CreateListing from "./pages/CreateListing";
